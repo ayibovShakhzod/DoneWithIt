ee489850f6ac6e7fbcc16de99c7ea0f5b0f2890c
Shakhzod <ayubjonovshakhzod01@gmail.com>
2021-01-31 14:41:50 +0500

    App Picker, App text Input, Picker Item and styles

--- App.js
+++ App.js
@@ -1,13 +1,31 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import Icon from './app/components/Icon';
-import ListItem from './app/components/ListItem';
+import React, { useState } from 'react';
+import {
+  View,
+  StyleSheet,
+  TextInput,
+  Text,
+  Switch
+} from 'react-native';
+import AppPicker from './app/components/AppPicker';
+import AppTextInput from './app/components/AppTextInput';
 import Screen from './app/components/Screen';
-import AccountScreen from './app/screens/AccountScreen';
-import ListingScreen from './app/screens/ListingScreen';
-
+const categories = [
+  { label: 'Furniture', value: 1 },
+  { label: 'Clothing', value: 2 },
+  { label: 'Cameras', value: 3 }
+];
 export default function App() {
+  const [category, setCategory] = useState(categories[0]);
   return (
-    <ListingScreen />
+    <Screen>
+      <AppPicker
+        selectedItem={category}
+        onSelectItem={(item) => setCategory(item)}
+        items={categories}
+        icon="apps"
+        placeholder="Category"
+      />
+      <AppTextInput icon="email" placeholder="Email" />
+    </Screen>
   );
 }
